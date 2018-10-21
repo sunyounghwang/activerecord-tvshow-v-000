95323a484bf93578ed731a4972721ec63803d204
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-21 18:10:28 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -4,7 +4,7 @@ class CreateShows < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :network
       t.string :day
-      t.integer :rating    
+      t.integer :rating
     end
   end
 end
